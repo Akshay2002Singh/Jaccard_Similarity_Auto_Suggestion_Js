e3d97e55e676968f5849695fdf0e453479d157d7
Akshay2002Singh <06akshay2002@gmail.com>
2022-02-26 13:22:27 +0530

    creating dummy data

---
+++ algo.js
@@ -0,0 +1,10 @@
+// User    Python C C++ Node Css Html Django Kotlin Pandas Numpy Jupyter Js React
+// user1     1    0  1    0    1   1     1      1      1     1      1     1   1
+// user1     1    0  1    0    1   1     0      0      1     0      1     0   0
+// user1     0    0  1    1    0   1     1      0      0     0      1     1   0
+let user_data = {
+    user1 = [1,0,1,0,1,1,1,1,1,1,1,1,1],
+    user2 = [1,0,1,0,1,1,0,0,1,0,1,0,0],
+    user3 = [0,0,1,1,0,1,1,0,0,0,1,1,0],
+    user4 = [0,1,0,1,0,1,1,0,0,1,1,0,1]
+}
