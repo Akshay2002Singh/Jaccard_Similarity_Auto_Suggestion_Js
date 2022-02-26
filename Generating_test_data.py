8f66efce2694740747a36384da5344e3047c86c1
Akshay2002Singh <06akshay2002@gmail.com>
2022-02-26 14:19:53 +0530

    algorithm ready

--- Generating_test_data.py
+++ Generating_test_data.py
@@ -4,4 +4,4 @@ def ran():
     n = random.randint(0,1)
     return n
 for i in range(50):
-    print(f"user{5+i} = [{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()}],")
+    print(f"user{5+i} : [{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()},{ran()}],")
