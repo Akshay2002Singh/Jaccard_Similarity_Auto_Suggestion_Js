0c498045f43ba5450eaf2e497a7dc8f51ce9c13d
Akshay2002Singh <06akshay2002@gmail.com>
2022-02-26 14:04:54 +0530

    created new function

--- algo.js
+++ algo.js
@@ -3,16 +3,16 @@
 // user1     1    0  1    0    1   1     0      0      1     0      1     0   0
 // user1     0    0  1    1    0   1     1      0      0     0      1     1   0
 var user_data = {
-    user1 : [1,0,1,0,1,1,1,1,1,1,1,1,1],
-    user2 : [1,0,1,0,1,1,0,0,1,0,1,0,0],
+    user1 : [1,1,1,0,1,1,1,1,1,1,1,1,1],
+    user2 : [1,1,1,0,1,1,0,0,1,0,1,0,1],
     user3 : [0,0,1,1,0,1,1,0,0,0,1,1,0],
-    user4 : [0,1,0,1,0,1,1,0,0,1,1,0,1]
+    user4 : [0,1,0,1,0,1,1,0,1,1,1,0,1]
 };
 
 let new_user = [0,1,0,0,0,0,1,1,0,0,0,0,1];
 
 
-function sum_logical_or(x,y){
+function sum_logical_and(x,y){
     let count = 0;
     for(let i =0;i<x.length ;++i){
         if(x[i]==1 && y[i]==1){
@@ -21,6 +21,21 @@ function sum_logical_or(x,y){
     }
     return count;
 }
+function sum_logical_or(x,y){
+    let count = 0;
+    // for(let i =0,j=0;i<x.length || j<y.length ;++i,++j){
+    //     count+=1;
+    // }
+    count = 13;
+    // we are using constant number because number of tags are fix and does not changes with our data 
+    return count;
+}
 
-c = sum_logical_or(user_data['user1'],new_user);
-console.log(c)
+similarities = {}
+for (user in user_data){
+    similarities[user] = sum_logical_and(user_data[user],new_user)/sum_logical_or(user_data[user],new_user)
+}
+
+for (user in similarities){
+    console.log(`${user} = ${similarities[user]}`)
+}
